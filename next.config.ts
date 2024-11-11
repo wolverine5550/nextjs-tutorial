0f22c8f7e77997e1efc27fdfb7672640d858aad0
wolverine5550 <apascar@gmail.com>
2024-11-11 12:07:44 -0500

    Adding Search and Pagination

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    ppr: 'incremental',
+    ppr: "incremental",
   },
   /* config options here */
 };
